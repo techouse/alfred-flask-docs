3e329dc30ca6845b1121bf658cc336627c8344a1
Klemen Tušar <techouse@gmail.com>
2022-05-21 14:29:54 +0100

    :arrow_up: Add support for Flask 2.1.x

--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,7 +1,7 @@
 class Config {
   Config._();
 
-  static const String version = '2.0.9';
+  static const String version = '2.1.0';
   static final Uri githubRepositoryUrl =
       Uri.https('github.com', '/techouse/alfred-flask-docs');
   static const String algoliaApplicationId = 'WODHKE4WZG';
@@ -13,5 +13,6 @@ class Config {
     'v1',
     'v1.1',
     'v2',
+    'v2.1',
   ];
 }
