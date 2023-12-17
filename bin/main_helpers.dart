dcf70ebc79ad414f023a2b3a5d8f06dd1fef5f6f
Klemen Tušar <techouse@gmail.com>
2023-12-17 19:07:47 +0000

    :alien: replace dart_algolia with algoliasearch

--- bin/main_helpers.dart
+++ bin/main_helpers.dart
@@ -29,48 +29,56 @@ void _showPlaceholder() {
 }
 
 Future<void> _performSearch(String query, {String? version}) async {
-  final AlgoliaQuerySnapshot snapshot = await AlgoliaSearch.query(
-    query,
-    version: version,
-  );
-
-  if (snapshot.nbHits > 0) {
-    final AlfredItems items = AlfredItems(
-      snapshot.hits.map((snapshot) => SearchResult.fromJson(snapshot.data)).map(
-        (result) {
-          return AlfredItem(
-            uid: result.objectID,
-            title: result.id,
-            subtitle: result.content.truncate(75),
-            arg: result.permalink,
-            text: AlfredItemText(
-              largeType: result.title,
-              copy: result.permalink,
-            ),
-            quickLookUrl: result.permalink,
-            icon: AlfredItemIcon(path: 'icon.png'),
-            valid: true,
-          );
-        },
-      ).toList(),
+  try {
+    final SearchResponse searchResponse = await AlgoliaSearch.query(
+      query,
+      version: version,
     );
-    _workflow.addItems(items.items);
-  } else {
-    final Uri url =
-        Uri.https('www.google.com', '/search', {'q': 'Flask $query'});
 
-    _workflow.addItem(
-      AlfredItem(
-        title: 'No matching answers found',
-        subtitle: 'Shall I try and search Google?',
-        arg: url.toString(),
-        text: AlfredItemText(
-          copy: url.toString(),
+    if (searchResponse.nbHits > 0) {
+      final AlfredItems items = AlfredItems(
+        searchResponse.hits
+            .map((Hit hit) =>
+            SearchResult.fromJson(
+                <String, dynamic>{...hit, 'objectID': hit.objectID}))
+            .map(
+              (result) =>
+              AlfredItem(
+                uid: result.objectID,
+                title: result.id,
+                subtitle: result.content.truncate(75),
+                arg: result.permalink,
+                text: AlfredItemText(
+                  largeType: result.title,
+                  copy: result.permalink,
+                ),
+                quickLookUrl: result.permalink,
+                icon: AlfredItemIcon(path: 'icon.png'),
+                valid: true,
+              ),
+        )
+            .toList(),
+      );
+      _workflow.addItems(items.items);
+    } else {
+      final Uri url =
+      Uri.https('www.google.com', '/search', {'q': 'Flask $query'});
+
+      _workflow.addItem(
+        AlfredItem(
+          title: 'No matching answers found',
+          subtitle: 'Shall I try and search Google?',
+          arg: url.toString(),
+          text: AlfredItemText(
+            copy: url.toString(),
+          ),
+          quickLookUrl: url.toString(),
+          icon: AlfredItemIcon(path: 'google.png'),
+          valid: true,
         ),
-        quickLookUrl: url.toString(),
-        icon: AlfredItemIcon(path: 'google.png'),
-        valid: true,
-      ),
-    );
+      );
+    }
+  } finally {
+    AlgoliaSearch.dispose();
   }
 }
